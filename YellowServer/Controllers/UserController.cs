110600dd4bf7cf6ece708deb3ad40ddcac7d70f0
Rajkiran Panuganti <rajkiran.panuganti@gmail.com>
2018-04-22 16:12:21 -0700

    Getting Started

---
+++ YellowServer/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Http;
+
+namespace YellowServer.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET api/user/GetUserInfo/request
+        [HttpGet]
+        public void GetUserInfo(string request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidateCredentials(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YellowServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YellowServer.WebApiApplication" Language="C#" %>
